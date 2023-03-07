79562a03e977b495e8362d45fed5f3a06e54ac96
Tatiana <t-b-k@bk.ru>
2023-03-07 17:57:48 +0300

    Added all tasks from Homework 3

--- SEMINAR3/HMW3/HMW3_Task3.py
+++ SEMINAR3/HMW3/HMW3_Task3.py
@@ -25,12 +25,75 @@
 # только английские или только русские буквы
 
 # РЕШЕНИЕ: 
-# Введенное пользователем слово разобьем на отдельные символы. 
-# Создадим словари: русский и английский, в которых ключом буду являться символы 
-# соответствующего алфавита, а значением - их вес. 
-# Пройдемся по нашему списку символов и для каждого встреченного символа прибавим 
-# к результату его вес
+# Введенное пользователем слово - это строка. Сделаем все буквы большими - upper(). 
+# Создадим словари: русский и английский, в которых ключом буду являться строки 
+# символов соответствующего алфавита, имеющих одинаковый вес, а значением - сам их вес. 
+# Сначала по первому символу введенного слова определим язык. 
+# Затем пройдемся по нашему слову и для каждого встреченного символа прибавим 
+# к результату его вес, найденный в соответствующем словаре
 
-word = input("Введите слово для анализа его стоимости => ")
+# I вариант: 
 
-dict_rus = 
+word = input("Введите слово для анализа его стоимости => ").upper()
+print(word)
+
+dict_rus = {"АВЕИНОРСТ": 1, "ДКЛМПУ": 2, "БГЁЬЯ": 3, "ЙЫ": 4, "ЖЗХЦЧ": 5, "ШЭЮ": 8, "ФЩЪ": 10}
+dict_eng = {"AEIOULNSTR": 1, "DG": 2, "BCMP": 3, "FHVWY": 4, "K": 5, "JX": 8, "QZ": 10}
+
+# Для каждого символа во веденной пользователем строке прибавим к результату 
+# его вес, найденный по ключу в соответствующем словаре
+
+language = "Русский"
+for item in dict_eng : 
+    if word[0] in item : 
+        language = "Английский"
+        break
+
+print(f"Ваш язык - {language}")
+
+weight_of_word = 0
+if language == "Русский" : 
+    for symbol in word : 
+        for item in dict_rus : 
+            if symbol in item : 
+                weight_of_word += dict_rus[item]
+else : 
+    for symbol in word : 
+        for item in dict_eng : 
+            if symbol in item : 
+                weight_of_word += dict_eng[item]
+    
+print(f"Вес введенного Вами слова равен {weight_of_word}")
+
+# II вариант: с использованием comprehensions (сокращенная форма for)
+
+word = input("Введите слово для анализа его стоимости => ").upper()
+print(word)
+
+dict_rus = {"АВЕИНОРСТ": 1, "ДКЛМПУ": 2, "БГЁЬЯ": 3, "ЙЫ": 4, "ЖЗХЦЧ": 5, "ШЭЮ": 8, "ФЩЪ": 10}
+dict_eng = {"AEIOULNSTR": 1, "DG": 2, "BCMP": 3, "FHVWY": 4, "K": 5, "JX": 8, "QZ": 10}
+
+language = "Русский"
+for item in dict_eng : 
+    if word[0] in item : 
+        language = "Английский"
+        break
+
+print(f"Ваш язык - {language}")
+if language == "Русский" : 
+    weight_of_word = sum([item[1] for item in dict_rus.items() for symbol 
+                          in word if symbol in item[0]])
+    # for symbol in word : 
+    #     for item in dict_rus : 
+    #         if symbol in item : 
+    #             weight_of_word += dict_rus[item]
+else : 
+    weight_of_word = sum([item[1] for item in dict_eng.items() for symbol 
+                          in word if symbol in item[0]])
+    # for symbol in word : 
+    #     for item in dict_eng : 
+    #         if symbol in item : 
+    #             weight_of_word += dict_eng[item]
+
+print(f"Вес введенного Вами слова равен {weight_of_word}")
+
